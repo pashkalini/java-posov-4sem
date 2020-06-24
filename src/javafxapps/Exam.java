f4a4ff4aa5da7e8b4ccfe831ad2331bccac3632f
pashkalini <pashkalini@users.noreply.github.com>
2020-06-24 12:11:20 +0300

    Тест к экзамену

---
+++ src/javafxapps/Exam.java
@@ -0,0 +1,33 @@
+package javafxapps;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+// тест к экзамену
+public class Exam extends Application {
+    @Override
+    public void start(Stage mainStage) {
+        Scene scene = new Scene(initInterface(), 660, 480);
+        mainStage.setScene(scene);
+        mainStage.show();
+    }
+
+    private VBox initInterface() {
+        VBox vb = new VBox();
+        Button b = new Button("Hello");
+        TextField tf = new TextField();
+        Label l = new Label("Some text");
+        b.addEventHandler(ActionEvent.ACTION, ActionEvent -> {
+            l.setText(tf.getText());
+        });
+
+        vb.getChildren().addAll(b, tf, l);
+        return vb;
+    }
+}
